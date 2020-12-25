df9e0ea6763d25fd041be629c40db7aca3cd9500
Erik Marsh <erik.i.marsh@gmail.com>
2020-12-24 21:19:38 -0800

    added memory parameter to run script

--- run_tests.ps1
+++ run_tests.ps1
@@ -1,9 +1,9 @@
 Write-Host "=== Hello world tests ==="
-.\build\src\brainfuck.exe .\test\hello.bf
-.\build\src\brainfuck.exe .\test\rdebath\Brainfuck\testing\Hello.b
-.\build\src\brainfuck.exe .\test\rdebath\Brainfuck\testing\Hello2.b
+.\build\src\brainfuck.exe .\test\hello.bf --memory=30000
+.\build\src\brainfuck.exe .\test\rdebath\Brainfuck\testing\Hello.b --memory=30000
+.\build\src\brainfuck.exe .\test\rdebath\Brainfuck\testing\Hello2.b --memory=30000
 Write-Host "=== Golden ratio ==="
-.\build\src\brainfuck.exe .\test\rdebath\Brainfuck\testing\Golden.b
+.\build\src\brainfuck.exe .\test\rdebath\Brainfuck\testing\Golden.b --memory=30000
 Write-Host ""
 Write-Host "=== al-count-1.b ==="
-.\build\src\brainfuck.exe .\test\rdebath\Brainfuck\testing\al-count-1.b
+.\build\src\brainfuck.exe .\test\rdebath\Brainfuck\testing\al-count-1.b --memory=30000
